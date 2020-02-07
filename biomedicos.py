d499ffe8ea362692dc9e4d20c932dbacee1851e0
GerardotCol <ge.dardo@gmail.com>
2020-02-06 18:35:52 -0600

    Lista de oferta de trabajos

--- biomedicos.py
+++ biomedicos.py
@@ -1,14 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
+import csv
 
-print(sys.version)
+# static HTML content
 
+# URL = "https://www.occ.com.mx/empleos/de-ingenieria-biomedica/en-mexico/"
+url_1 = "https://www.computrabajo.com.mx/ofertas-de-trabajo/p="
+location = "estado-de-mexico"
+empleo = "programador"
 
-URL = "https://www.occ.com.mx/empleos/de-ingenieria-biomedica/en-mexico/"
+csv_file = open("cms_scrape.csv", "w")
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(["Vacante", "Resumen", "Empresa", "Estado"])
 
-page = requests.get(URL)
+# results = soup.find(id="ResultsContainer")
+# print(soup.find(class="bRS bClick ")){}{}[]
+for i in range(1, 10):
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
-print(results)
+    url = (
+        "https://www.computrabajo.com.mx/ofertas-de-trabajo/?p="
+        + str(i)
+        + "&q="
+        + str(empleo)
+    )
+    print(url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    for oferta in soup.find_all("div", class_="bRS bClick"):
+        vacante = oferta.h2.text
+        resumen = oferta.p.text
+        empresa = oferta.div.div.a.text
+        estado = oferta.div.div.span.next_sibling.next_sibling.a.text
+
+        csv_writer.writerow([vacante, resumen, empresa, estado])
+
+csv_file.close()
+
